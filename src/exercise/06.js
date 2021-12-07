6334c8940cfbcc35c809e998aebf1def01bc50e6
joonmo-you <joonmo.you@gmail.com>
2021-12-07 18:49:32 +0900

    06-extra2

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -105,6 +105,10 @@ Grid = React.memo(Grid)
 function Cell({row, column}) {
   const state = useAppState()
   const cell = state.grid[row][column]
+  return <CellImpl cell={cell} row={row} column={column} />
+}
+
+function CellImpl({cell, row, column}) {
   const dispatch = useAppDispatch()
   const handleClick = () => dispatch({type: 'UPDATE_GRID_CELL', row, column})
   return (
@@ -120,7 +124,7 @@ function Cell({row, column}) {
     </button>
   )
 }
-Cell = React.memo(Cell)
+CellImpl = React.memo(CellImpl)
 
 function DogNameInput() {
   const [state, dispatch] = useDogState()
